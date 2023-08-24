96dac4a45ed796e72b29aedf5db1b5d68998521b
Saqib Bhatti <saqibbhatti.public@gmail.com>
2023-08-24 17:29:26 +0200

    basic backend hello world endpoint

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+//modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//home route
+app.get("/", (req, res) => {    
+    res.send("Hello World");
+}
+);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
